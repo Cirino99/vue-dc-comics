283a60c6bcaf85ca24ea3daf8e167407b5da4ed1
Simone <cirino.simone99@gmail.com>
2022-06-08 16:10:25 +0200

    completato componente header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,123 @@
 <template>
-  <header>
-      
-  </header>
+    <header>
+        <nav>
+            <img :src="logo" alt="">
+            <ul>
+                <li v-for="(link,index) in links" :key="index">
+                    <a :class="link.current ? 'active' : ''" :href="link.url">
+                        {{link.text}}
+                        <div v-if="link.current"></div>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
 </template>
 
 <script>
 export default {
-  name: 'HeaderVue'
+    name: 'HeaderVue',
+    data(){
+        return {
+            logo: require('../assets/img/dc-logo.png'),
+            links: [
+                {
+                    text: 'CHARACTERS',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'COMICS',
+                    url: '#',
+                    current: true
+                },
+                {
+                    text: 'MOVIES',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'TV',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'GAMES',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'COLLECTIBLES',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'VIDEOS',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'FANS',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'NEWS',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'SHOP',
+                    url: '#',
+                    current: false
+                }
+            ]
+        }
+    }
 }
 </script>
 
 <style scoped lang="scss">
+header {
+    height: 120px;
+    display: flex;
+    align-items: center;
+    nav {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 70%;
+        height: 100%;
+        margin: 0 auto;
+        
+        ul {
+            list-style-type: none;
 
+            li {
+                display: inline-block;
+                margin: 0 10px;
+
+                a {
+                    text-decoration: none;
+                    color: #464646;
+                    position: relative;
+                    font-weight: 600;
+
+                    &.active {
+                        color: #0282f9;
+                    }
+
+                    div {
+                        position: absolute;
+                        height: 5px;
+                        width: 100%;
+                        bottom: -51px;
+                        left: 0;
+                        background-color: #0282f9;
+                    }
+                }
+            }
+        }
+    }
+}
 </style>
