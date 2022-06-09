9f89a76aace6585983d052636aa4f582aad6297e
Simone <cirino.simone99@gmail.com>
2022-06-09 16:21:38 +0200

    prima versione serie.vue

--- src/components/Serie.vue
+++ src/components/Serie.vue
@@ -1,15 +1,30 @@
 <template>
-    <div>
-        
+    <div class="card-serie">
+        <img :src="serie.thumb" alt="serie.series">
+        <span>{{serie.series}}</span>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'SerieVue'
+    name: 'SerieVue',
+    props : {
+    serie: Object
+  }
 }
 </script>
 
 <style scoped lang="scss">
+.card-serie {
+    width: calc(100% / 6);
+    padding: 10px;
+    margin-top: 30px;
 
+    img {
+        width: 100%;
+        max-height: 200px;
+        object-fit: cover;
+        object-position: top;
+    }
+}
 </style>
